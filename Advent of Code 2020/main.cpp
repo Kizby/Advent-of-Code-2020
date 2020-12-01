d9f7ee8e3d307913c994f1b9cb7fb81ac7e3aebe
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+GitKraken@gmail.com>
2020-12-01 08:24:19 -0500

    Add report and copy to streamline getting answer on clipboard

--- Advent of Code 2020/main.cpp
+++ Advent of Code 2020/main.cpp
@@ -1,15 +1,18 @@
-﻿#include <fstream>
+﻿#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <filesystem>
 #include <set>
 #include <sstream>
 #include <algorithm>
+extern "C" {
+#include <Windows.h>
+}
 
 #define YEAR "2020"
-
-#define CPPHTTPLIB_OPENSSL_SUPPORT
-#include "httplib.h"
 
 using namespace std;
 using namespace httplib;
@@ -99,27 +102,72 @@ unordered_map<T, int> histogram(C container) {
 	return result;
 }
 
+void copy(string s)
+{
+	if (s.size() > 200) {
+		// probably don't want to copy this...
+		cout << "String is too long: " << s.size() << endl;
+		return;
+	}
+
+	// Open the clipboard, and empty it. 
+	if (!OpenClipboard(NULL))
+		return;
+	EmptyClipboard();
+
+	// Copy text using the CF_TEXT format. 
+	// Allocate a global memory object for the text. 
+	HGLOBAL hglbCopy = GlobalAlloc(GMEM_MOVEABLE,
+		(s.size() + 1) * sizeof(TCHAR));
+
+	// Lock the handle and copy the text to the buffer. 
+	LPTSTR lptstrCopy = (LPTSTR)GlobalLock(hglbCopy);
+	memcpy(lptstrCopy, s.c_str(), s.size() * sizeof(TCHAR));
+	lptstrCopy[s.size()] = '\0';
+	GlobalUnlock(hglbCopy);
+
+	// Place the handle on the clipboard. 
+	SetClipboardData(CF_TEXT, hglbCopy);
+
+	// Close the clipboard. 
+	CloseClipboard();
+}
+
+void report(const string& s) {
+	cout << s << endl;
+	copy(s);
+}
+
+template <typename T>
+void report(T n) {
+	report(to_string(n));
+}
+
 void day1() {
 	auto in = input("1");
 	auto nums = map_to_num(split(slurp(in)));
+	int64_t part1 = 0;
+	int64_t part2 = 0;
 	for (auto num : nums) {
 		for (auto num2 : nums) {
 			if (num == num2) {
 				continue;
 			}
 			if (num + num2 == 2020) {
-				cout << num * num2 << endl;
+				part1 = num * num2;
 			}
 			for (auto num3 : nums) {
 				if (num3 == num2 || num3 == num) {
 					continue;
 				}
 				if (num + num2 + num3 == 2020) {
-					cout << num * num2 * num3 << endl;
+					part2 = num * num2 * num3;
 				}
 			}
 		}
 	}
+	report(part1);
+	report(part2);
 }
 
 void day2() {
