1f0fcc9d43bd593e5b35770852dd26fa2b600fc4
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+GitKraken@gmail.com>
2020-11-30 19:42:23 -0500

    Prep for day 1

--- Advent of Code 2020/main.cpp
+++ Advent of Code 2020/main.cpp
@@ -4,8 +4,9 @@
 #include <filesystem>
 #include <set>
 #include <sstream>
+#include <algorithm>
 
-#define YEAR "20XX"
+#define YEAR "2020"
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
@@ -44,7 +45,7 @@ ifstream input(string day) {
 }
 
 // from https://stackoverflow.com/a/9437426
-vector<string> split(const string& input, const string& regex) {
+vector<string> split(const string& input, const string& regex = "\n") {
 	// passing -1 as the submatch index parameter performs splitting
 	std::regex re(regex);
 	std::sregex_token_iterator
@@ -54,7 +55,7 @@ vector<string> split(const string& input, const string& regex) {
 }
 
 // from https://stackoverflow.com/a/116083
-string slurp(ifstream &in) {
+string slurp(ifstream& in) {
 	stringstream out;
 	while (in >> out.rdbuf());
 	string s = out.str();
@@ -62,5 +63,46 @@ string slurp(ifstream &in) {
 	return s.substr(0, last_non_whitespace + 1);
 }
 
+vector<int64_t> map_to_num(const vector<string>& vec) {
+	vector<int64_t> result;
+	result.resize(vec.size());
+	transform(vec.begin(), vec.end(), result.begin(), [](const string& s) { return stoll(s); });
+	return result;
+}
+
+vector<vector<size_t>> permutations(size_t start, size_t end) {
+	if (start >= end) {
+		return vector<vector<size_t>>{vector<size_t>{}};
+	}
+	auto base = permutations(start, end - 1);
+	vector<vector<size_t>> result;
+	for (auto elem : base) {
+		for (int i = 0; i <= elem.size(); ++i) {
+			auto next = elem;
+			next.insert(next.begin() + i, end - 1);
+			result.push_back(move(next));
+		}
+	}
+	return result;
+}
+
+vector<vector<size_t>> permutations(size_t n) {
+	return permutations(0, n);
+}
+
+template <typename T, typename C>
+unordered_map<T, int> histogram(C container) {
+	unordered_map<T, int> result;
+	for (T one : container) {
+		++result[one];
+	}
+	return result;
+}
+
+void day1() {
+
+}
+
 int main() {
+	day1();
 }
